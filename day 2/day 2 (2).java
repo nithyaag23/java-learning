e22f094ae6cccd4e1069ff51da72aae2f5cf1081
nithyaag23 <nithyaag234@gmail.com>
2026-07-07 21:10:02 +0530

    day 2 java learning

---
+++ day 2/day 2 (2).java
@@ -0,0 +1,10 @@
+class main
+{
+   public static void main(String a[])
+   {
+       boolean fun = true;
+       boolean bore = false;
+       System.out.println("is java fun?"+ fun);
+       System.out.println("is java boring?"+ bore);
+   }
+}
